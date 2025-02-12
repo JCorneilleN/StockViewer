e3e966390d18a9c6162348591edb49bd58a96299
Corneille <65753539+JCorneilleN@users.noreply.github.com>
2025-02-12 14:57:22 -0800

    added the static folder for the flask application object

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, url_for
 from StockDataRetriever import StockDataRetriever
 
 # Create flask application object
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static')
 
 
 # Connect to database and return connection object
